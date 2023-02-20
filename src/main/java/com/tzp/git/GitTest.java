5422c7d072b2d79fb4ac01ff89cbc1ed8bc5fa7a
eden <tzp18391721907@163.com>
2023-02-20 11:23:01 +0800

    first commit HelloWorld

---
+++ src/main/java/com/tzp/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.tzp.git;
+
+/**
+ * GitTest:
+ *
+ * @author eden
+ * @date 2023/02/20
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
